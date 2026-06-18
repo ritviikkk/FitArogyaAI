797d4b07341a9aa9618f0ed1e4402255af9cc7c0
RITVIK KHADAGDE <r8877416@gmail.com>
2026-06-18 13:03:11 +0530

    Fix GitHub Pages asset loading

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FitArogyaAI/'
+  base: '/FitArogyaAI/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: (assetInfo) =>
+          assetInfo.name?.endsWith('.css') ? 'assets/index.css' : 'assets/[name][extname]',
+      },
+    },
+  },
 })
